afe6cdf267c0eaad46d6caae4aaa57615819ce35
mnxzywod3660-collab <mnxzywod3660@gmail.com>
2026-07-03 03:26:54 +0300

    Refactor supabase client initialization

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,9 @@
 const SUPABASE_URL = "https://qotfbplwpmernvwpzalm.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_f-rZ31A49z5wm5nyBiSLHA_8bLNoyzE";
 
-const supabase = window.supabase.createClient(
+const { createClient } = window.supabase;
+
+supabase = createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
